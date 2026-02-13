c82d09d063e3bdcc42a070ad8370dec62ab9ee6e
breezyfc <pscott1097@gmail.com>
2026-02-13 12:01:11 -0500

    changed db to where "course id" has to be 4 numbers long (to follow etsu guidelines of course id)

--- app/models.py
+++ app/models.py
@@ -1,13 +1,22 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, CheckConstraint
 from app.database import Base
 
 class Assignment(Base):
     __tablename__ = "assignments"
 
     id = Column(Integer, primary_key=True, index=True)
+
+    course_id = Column(String(4), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(
+            "course_id GLOB '[0-9][0-9][0-9][0-9]'",
+            name="course_id_must_be_4_digits"
+        ),
+    )
+
     name = Column(String, index=True)
     course = Column(String)
-    course_id = Column(Integer)
     due_date = Column(String)
     due_time = Column(String)
     assignment_type = Column(String)
