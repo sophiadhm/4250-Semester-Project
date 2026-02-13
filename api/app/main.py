cf3e7a472b883db12049d5fbcfb185ea4d61f82c
breezyfc <pscott1097@gmail.com>
2026-02-13 11:05:06 -0500

    updated main with put, patch, post, delete

--- api/app/main.py
+++ api/app/main.py
@@ -7,3 +7,40 @@ from app.models import Assignment
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+@app.post("/assignments/", status_code=status.HTTP_201_CREATED, response_model=AssignmentResponse)
+def create_assignment(assignment: AssignmentCreate, db: Session = Depends(get_db)):
+    db_assignment = Assignment(name = assignment.name, course= assignment.course, course_id= assignment.course_id, due_date= assignment.due_date, due_time= assignment.due_time, assignment_type= assignment.assignment_type, priority_level= assignment.priority_level, points= assignment.points)
+    db_assignment = Assignment(**assignment.dict())
+    db.add(db_assignment)
+    db.commit()
+    db.refresh(db_assignment)
+    return db_assignment
+
+@app.get("/assignments/{assignment_id}", response_model=AssignmentResponse)
+def read_assignment(assignment_id: int, db: Session = Depends(get_db)):
+    db_assignment = db.query(Assignment).filter(Assignment.id == assignment_id).first()
+    if db_assignment is None:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    return db_assignment
+
+@app.put("/assignments/{assignment_id}", response_model=AssignmentResponse)
+def update_assignment(assignment_id: int, assignment: AssignmentUpdate, db: Session = Depends(get_db)):
+    db_assignment = db.query(Assignment).filter(Assignment.id == assignment_id).first()
+    if db_assignment is None:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    for var, value in vars(assignment).items():
+        if value is not None:
+            setattr(db_assignment, var, value)
+    db.commit()
+    db.refresh(db_assignment)
+    return db_assignment
+
+@app.delete("/assignments/{assignment_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_assignment(assignment_id: int, db: Session = Depends(get_db)):
+    db_assignment = db.query(Assignment).filter(Assignment.id == assignment_id).first()
+    if db_assignment is None:
+        raise HTTPException(status_code=404, detail="Assignment not found")
+    db.delete(db_assignment)
+    db.commit()
+    return
