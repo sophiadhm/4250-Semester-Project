06ec1b8241a0aca796dcf7643ba302a2233316ab
breezyfc <pscott1097@gmail.com>
2026-02-13 10:55:41 -0500

    added classes to schemas file

--- api/app/main.py
+++ api/app/main.py
@@ -1,2 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from app.database import get_db, Base, engine
+from app.schemas import AssignmentCreate, AssignmentUpdate, AssignmentResponse
+from app.models import Assignment
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
