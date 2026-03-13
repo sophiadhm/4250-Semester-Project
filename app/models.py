2ae2b91b1282ff46a6171a83e1300a1e9d5151bd
breezyfc <pscott1097@gmail.com>
2026-03-13 13:13:33 -0400

    reworked assignment logic

--- app/models.py
+++ app/models.py
@@ -1,41 +1,33 @@
-from sqlalchemy import Column, Integer, String, Float, CheckConstraint
-from app.database import Base
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     ics_url = db.Column(db.String(500))
-    def set_password(self, password):        
+
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
+
 class Assignment(db.Model):
     __tablename__ = "assignments"
-
     id = db.Column(db.Integer, primary_key=True, index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)  
     course_id = db.Column(db.String(4), nullable=False)
-    __table_args__ = (
-        db.CheckConstraint(
-            "course_id GLOB '[0-9][0-9][0-9][0-9]'",
-            name="course_id_must_be_4_digits"
-        ),
-    )
-
     name = db.Column(db.String, index=True)
     course = db.Column(db.String)
     due_date = db.Column(db.String)
     due_time = db.Column(db.String)
-    assignment_type = Column(String)
-    priority_level = Column(Integer)
-    points = Column(Float)
+    assignment_type = db.Column(db.String)
+    priority_level = db.Column(db.Integer)
+    points = db.Column(db.Float)
